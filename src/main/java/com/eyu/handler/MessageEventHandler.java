0f54e0cd58af271d9920f398a4e4325be3f16dec
zqzq3 <cloutlook@qq.com>
2023-03-30 15:49:52 +0800

    fix:修复sender id取值错误的问题

--- src/main/java/com/eyu/handler/MessageEventHandler.java
+++ src/main/java/com/eyu/handler/MessageEventHandler.java
@@ -61,7 +61,7 @@ public class MessageEventHandler implements ListenerHost {
         if (event.getBot().getGroups().contains(event.getSubject().getId())) {
             //如果是在群聊
             if (event.getMessage().contains(new At(event.getBot().getId()))) {
-                chatBO.setSessionId(String.valueOf(event.getBot().getId()));
+                chatBO.setSessionId(String.valueOf(event.getSender().getId()));
                 //存在@机器人的消息就向ChatGPT提问
                 //去除@再提问
                 String prompt = event.getMessage().contentToString().replace("@" + event.getBot().getId(), "").trim();
