8ceedb4e1b7e7efdf348b688f7ddec3496c375a0
zqzq3 <cloutlook@qq.com>
2023-04-12 16:25:18 +0800

    Revert "feat:更换为socks代理"
    This reverts commit f4d8da8dde0765771bd3375257f422d6c1392af9.

--- src/main/java/com/eyu/config/AccountConfig.java
+++ src/main/java/com/eyu/config/AccountConfig.java
@@ -46,10 +46,12 @@ public class AccountConfig {
     public void init() {
         //配置代理
         if (null != proxyConfig.getHost() && !"".equals(proxyConfig.getHost())) {
-            System.setProperty("socksProxyHost", proxyConfig.getHost());
+            System.setProperty("http.proxyHost", proxyConfig.getHost());
+            System.setProperty("https.proxyHost", proxyConfig.getHost());
         }
         if (null != proxyConfig.getPort() && !"".equals(proxyConfig.getPort())) {
-            System.setProperty("socksProxyPort", String.valueOf(proxyConfig.getPort()));
+            System.setProperty("http.proxyPort", proxyConfig.getPort());
+            System.setProperty("https.proxyPort", proxyConfig.getPort());
         }
         //ChatGPT
         model = "gpt-3.5-turbo";
